0891d3d60940cfc1a9a4637a36dd40c1218130f5
serafeimseroglou <seroglou@gmail.com>
2021-10-19 20:56:25 +0300

    test

--- main.py
+++ main.py
@@ -1,24 +1,71 @@
 def on_received_number(receivedNumber):
-    Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO1,
-        Math.map(receivedNumber, -1024, 1024, 0, 180))
+    if ready == 1:
+        Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO2,
+            Math.map(receivedNumber, 0, 1024, 0, 180))
+        basic.clear_screen()
+        led.plot(0, Math.map(receivedNumber, 0, 1024, 0, 9))
 radio.on_received_number(on_received_number)
 
 def on_button_pressed_a():
     global a
-    a = 0
+    a += -1
     Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO2, a)
-    basic.show_string("" + str((Math.map(a, 0, 180, 0, 9))))
+    basic.clear_screen()
+    led.plot(0, Math.map(a, 23, 40, 0, 9))
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
     global a
-    a += 20
-    basic.show_string("" + str((Math.map(a, 0, 180, 0, 9))))
+    a += 1
     Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO2, a)
+    basic.clear_screen()
+    led.plot(0, Math.map(a, 23, 40, 0, 9))
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 a = 0
+ready = 0
+ready = 0
 radio.set_group(1)
-a = 180
+a = 80
 Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO2, a)
-basic.show_string("" + str((Math.map(a, 0, 180, 0, 9))))
+basic.show_leds("""
+    . # # # .
+        . # . # .
+        . # . # .
+        . # . # .
+        . # # # .
+""")
+basic.pause(5000)
+a = 160
+Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO2, a)
+basic.show_leds("""
+    . # # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+""")
+basic.pause(5000)
+a = 20
+Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO2, a)
+basic.show_leds("""
+    . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+""")
+basic.pause(2000)
+a = 160
+Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO2, a)
+basic.pause(2000)
+a = 20
+Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO2, a)
+basic.show_leds("""
+    . . . . .
+        . . . . #
+        . . . # .
+        # . # . .
+        . # . . .
+""")
+ready = 1
