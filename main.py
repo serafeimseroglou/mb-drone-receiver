a9bd40382b9bca2622916c5872c441272fa61b3a
serafeimseroglou <seroglou@gmail.com>
2021-10-31 12:57:37 +0200

    Synchronizing motors ESCs

--- main.py
+++ main.py
@@ -1,32 +1,24 @@
 def on_received_number(receivedNumber):
     if ready == 1:
         Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO2,
-            Math.map(receivedNumber, 0, 1024, 0, 180))
-        basic.clear_screen()
-        led.plot(0, Math.map(receivedNumber, 0, 1024, 0, 9))
+            Math.map(receivedNumber, 0, 1024, 20, 160))
+        Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO1,
+            Math.map(receivedNumber, 0, 1024, 20, 160))
 radio.on_received_number(on_received_number)
 
 def on_button_pressed_a():
-    global a
-    a += -1
-    Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO2, a)
-    basic.clear_screen()
-    led.plot(0, Math.map(a, 23, 40, 0, 9))
+    pass
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
-    global a
-    a += 1
-    Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO2, a)
-    basic.clear_screen()
-    led.plot(0, Math.map(a, 23, 40, 0, 9))
+    pass
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-a = 0
 ready = 0
 ready = 0
 radio.set_group(1)
 a = 80
+Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO1, a)
 Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO2, a)
 basic.show_leds("""
     . # # # .
@@ -37,6 +29,7 @@ basic.show_leds("""
 """)
 basic.pause(5000)
 a = 160
+Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO1, a)
 Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO2, a)
 basic.show_leds("""
     . # # # .
@@ -47,6 +40,7 @@ basic.show_leds("""
 """)
 basic.pause(5000)
 a = 20
+Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO1, a)
 Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO2, a)
 basic.show_leds("""
     . . . . .
@@ -57,9 +51,11 @@ basic.show_leds("""
 """)
 basic.pause(2000)
 a = 160
+Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO1, a)
 Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO2, a)
 basic.pause(2000)
 a = 20
+Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO1, a)
 Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO2, a)
 basic.show_leds("""
     . . . . .
@@ -69,3 +65,7 @@ basic.show_leds("""
         . # . . .
 """)
 ready = 1
+
+def on_every_interval():
+    pass
+loops.every_interval(10, on_every_interval)
