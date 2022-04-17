673da299830ebc2288a99160dc3b05a874f4a71b
serafeimseroglou <seroglou@gmail.com>
2022-04-17 11:22:11 +0300

    Added 4th motor

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 input.onButtonPressed(Button.A, function () {
     a += 1
     b += 1
+    c += 1
     d += 1
     Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, a)
     Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo2, b)
+    Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo3, b)
     Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo4, d)
     basic.clearScreen()
     led.plot(0, Math.map(a, 10, 140, 0, 9))
@@ -11,9 +13,11 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     a += -1
     b += -1
+    c += -1
     d += -1
     Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, a)
     Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo2, b)
+    Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo3, b)
     Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo4, d)
     basic.clearScreen()
     led.plot(0, Math.map(a, 10, 140, 0, 9))
@@ -22,23 +26,35 @@ radio.onReceivedValue(function (name, value) {
     if (ready == 1) {
         if (name == "x") {
             Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, Math.map(value, 160, 1024, 10, 180))
-            Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo2, Math.map(value, 200, 1024, 10, 180))
+            Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo2, Math.map(value, 160, 1024, 10, 180))
         } else {
+            Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo3, Math.map(value, 50, 900, 10, 180))
             Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo4, Math.map(value, 50, 900, 10, 180))
         }
     }
 })
+/**
+ * TODO: 
+ * 
+ * - Make it lighter less than 300 grams
+ * 
+ * - Check independent 1 and 2 max threshold.
+ * 
+ * - Try to sync servo 1 and 2!
+ */
 let d = 0
 let b = 0
 let a = 0
 let ready = 0
 ready = 0
 radio.setGroup(1)
-a = 140
-b = 150
+a = 160
+b = 160
+let c = 160
 d = 160
 Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, a)
 Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo2, a)
+Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo3, a)
 Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo4, a)
 basic.showLeds(`
     . # # # .
@@ -65,10 +81,12 @@ basic.showLeds(`
     `)
 basic.pause(1000)
 a = 10
-b = 20
-d = 30
+b = 10
+c = 10
+d = 10
 Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, a)
 Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo2, b)
+Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo3, c)
 Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo4, d)
 basic.showLeds(`
     . . . . .
